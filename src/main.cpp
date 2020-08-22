409b8fcfdf7d06fc1c6629d076827413e3d36a21
wassfila <wassim.filali@gmail.com>
2020-08-22 20:00:20 +0200

    github library reference (note pio disk move issue)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-#include <ESPAsyncWebServer.h>
-#include <ESPAsyncWiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <ESPWiFiConfig.h>
 
 #include <ArduinoJson.h>
 #include "server_app.hpp"
@@ -10,13 +9,6 @@
 DynamicJsonDocument config(5*1024);//5 KB
 
 server_app server;
-
-void configModeCallback(AsyncWiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-}
 
 String get_config(){
   String result;
@@ -30,27 +22,8 @@ void set_config(String request){
   //TODO here update config with new_config
 }
 
-void wifi_setup(){
-  AsyncWebServer server(80);
-  DNSServer dns;
-  AsyncWiFiManager wifiManager(&server,&dns);
-
-  wifiManager.setAPCallback(configModeCallback);
-
-  if(!wifiManager.autoConnect()) {
-    Serial.println("failed to connect and hit timeout");
-
-    ESP.restart();
-    delay(1000);
-  }
-  Serial.println("WiFi connected");
-
-}
-
 void setup() {
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
 
   wifi_setup();
 
